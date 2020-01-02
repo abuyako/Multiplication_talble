7c26315279eb2aec128845ce4263564d9da45ed7
abuyako <abuyako@gmail.com>
2020-01-02 23:11:49 +0300

    Adding the if statement

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 var rows = prompt("How many rows for your mutliplication table");
 var cols = prompt("How many rows for your mutliplication table");
+if ( row == "" || rows == null )
+    rows = 10;
+if ( cols == "" || cols == null )
+    cols = 10;
+
